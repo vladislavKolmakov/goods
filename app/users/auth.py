722ea4bf427c1ff57a212659705a12df63857923
vladislavKolmakov <52298225+vladislavKolmakov@users.noreply.github.com>
2023-07-09 19:28:31 +0300

    add user signup

--- app/users/auth.py
+++ app/users/auth.py
@@ -1,7 +1,20 @@
 from passlib.context import CryptContext
+from pydantic import EmailStr
+from app.users.dao import UserDAO
 
 password_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
 def get_hashed_password(password: str) -> str:
     return password_context.hash(password)
+
+
+def verify_user(plain_password, verify_password) -> bool:
+    return password_context.verify(plain_password, verify_password)
+
+
+async def authentificate_user(email: EmailStr, password):
+    user = await UserDAO.find_one_or_none(email=email)
+    if not user and not verify_user(password, user.password):
+        return None
+    return user
--- app/users/router.py
+++ app/users/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from app.users.dao import UserDAO
 from app.users.schemas import SUserAuth, SUserReg
 from app.exeptions import UserAlreadyExistExeption
-from app.users.auth import get_hashed_password
+from app.users.auth import get_hashed_password, authentificate_user
 
 
 router = APIRouter(
@@ -16,6 +16,9 @@ async def create_user(user: SUserReg):
     existing_user = await UserDAO.find_one_or_none(email=user.email)
     if existing_user:
         raise UserAlreadyExistExeption
-    hashed_password = get_hashed_password(user.hashed_password)
-    user.hashed_password = hashed_password
+    user.hashed_password = get_hashed_password(user.hashed_password)
     await UserDAO.add(dict(user))
+
+@router.post('/signin')
+async def auth(user: SUserAuth):
+    return await authentificate_user(user.email, user.password)
